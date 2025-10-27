75c1ecc542a98b6da268c4fb50c3c45ad9fe89e1
PatricijaM09 <patrmame@madona.edu.lv>
2025-10-27 11:47:38 +0200

    Update uzd4.py

--- uzd4.py
+++ uzd4.py
@@ -5,6 +5,7 @@ skaitlis=1
 
 while skaitlis<=100:
     summma+=skaitlis # summma=summma+skaitlis
-    skaitlis=1 # nav vajadzīgs rakstīt otro reizi 
+    skaitlis+=1 
 
 print("Summa no 1 līdz 100 ir:summma}")
+
